c3f9120dacbabd549685799f65ffb2db20776a7d
Sean Peters <sp@snptrs.com>
2023-05-02 18:48:45 +0100

    Only show students starting with letter A

--- lib/directory.rb
+++ lib/directory.rb
@@ -20,7 +20,9 @@ end
 
 def print(students)
   students.each_with_index do |student, i|
+    if student[:name][0] == "A"
       puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 

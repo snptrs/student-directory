ace61a226ea1adcbd0144e76b59f329e90d0e783
Sean Peters <sp@snptrs.com>
2023-05-02 17:58:15 +0100

    Removed array of students

--- directory.rb
+++ directory.rb
@@ -1,17 +1,3 @@
-# students = [
-#   {name: "Dr. Hannibal Lecter", cohort: :november},
-#   {name: "Darth Vader", cohort: :november},
-#   {name: "Nurse Ratched", cohort: :november},
-#   {name: "Michael Corleone", cohort: :november},
-#   {name: "Alex DeLarge", cohort: :november},
-#   {name: "The Wicked Witch of the West", cohort: :november},
-#   {name: "Terminator", cohort: :november},
-#   {name: "Freddy Krueger", cohort: :november},
-#   {name: "The Joker", cohort: :november},
-#   {name: "Joffrey Baratheon", cohort: :november},
-#   {name: "Norman Bates", cohort: :november}
-# ]
-
 def input_students
   puts "Please enter the student names"
   puts "Hit return twice to finish"

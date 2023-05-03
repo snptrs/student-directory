b91c6dc3cbf7b95ab1eb87907fd3b600ee7f2214
Sean Peters <sp@snptrs.com>
2023-05-03 12:28:59 +0100

    Only show names > 12 chars

--- lib/directory.rb
+++ lib/directory.rb
@@ -20,7 +20,7 @@ end
 
 def print(students)
   students.each_with_index do |student, i|
-    if student[:name][0] == "A"
+    if student[:name].length < 12
       puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end

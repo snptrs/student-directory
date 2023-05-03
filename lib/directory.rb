f3ef003b09e9841e61b1e1dc522c8c6ccb327492
Sean Peters <sp@snptrs.com>
2023-05-03 17:33:39 +0100

    Refactoring

--- lib/directory.rb
+++ lib/directory.rb
@@ -1,16 +1,41 @@
+@students = []
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again."
+  end
+end
+
+def show_students
+  print_header
+  print_students
+  print_footer
+end
+
 def input_students
   puts "Please enter the student names"
   puts "Hit return twice to finish"
-  students = []
+  @students = []
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-
-  students
 end
 
 def print_header
@@ -18,17 +43,22 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index do |student, i|
+def print_students
+  @students.each_with_index do |student, i|
       puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  loop do
+    print_menu
+    selection = gets.chomp
+    process(selection)
+  end
+end
+
+interactive_menu

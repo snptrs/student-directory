6cfbf0f878071771c43e828e1b6b9d7a99993162
Sean Peters <sp@snptrs.com>
2023-05-03 12:47:05 +0100

    Adding student hobbies

--- lib/directory.rb
+++ lib/directory.rb
@@ -5,7 +5,9 @@ def input_students
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    puts "What's #{name}'s favourite hobby?"
+    hobby = gets.chomp
+    students << {name: name, cohort: :november, hobby: hobby}
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
@@ -21,7 +23,7 @@ end
 def print(students)
   count = 0
   while count < students.length
-      puts "#{count + 1}. #{students[count][:name]} (#{students[count][:cohort]} cohort)"
+      puts "#{count + 1}. #{students[count][:name]} (#{students[count][:cohort]} cohort), whose favourite hobby is #{students[count][:hobby]}"
       count += 1
   end
 end

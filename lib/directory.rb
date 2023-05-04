94aa9671fb2b98f4112b10c5b9a8d4fda87139db
Sean Peters <sp@snptrs.com>
2023-05-04 10:01:19 +0100

    Added method to write list to file

--- lib/directory.rb
+++ lib/directory.rb
@@ -3,6 +3,7 @@
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -12,6 +13,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -35,6 +38,16 @@ def input_students
     puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def print_header

a3b3f3a8dc50a6933afa1fcf27c8ff00c0292231
Sean Peters <sp@snptrs.com>
2023-05-05 09:26:05 +0100

    Tidying up formatting

--- lib/directory.rb
+++ lib/directory.rb
@@ -1,27 +1,28 @@
 require 'csv'
+
 @students = []
 
 def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to a file"
-  puts "4. Load the list from a file"
-  puts "9. Exit"
+  puts '1. Input the students'
+  puts '2. Show the students'
+  puts '3. Save the list to a file'
+  puts '4. Load the list from a file'
+  puts '9. Exit'
 end
 
 def process(selection)
   case selection
-  when "1"
+  when '1'
     input_students
-  when "2"
+  when '2'
     show_students
-  when "3"
+  when '3'
     filename = select_file
     save_students(filename)
-  when "4"
+  when '4'
     filename = select_file
     load_students(filename)
-  when "9"
+  when '9'
     exit
   else
     puts "I don't know what you meant, try again."
@@ -29,9 +30,9 @@ def process(selection)
 end
 
 def select_file
-  puts "Enter a filename,  or press return to use students.csv"
-  filename = STDIN.gets.chomp
-  filename.empty? ? "students.csv" : filename
+  puts 'Enter a filename,  or press return to use students.csv'
+  filename = $stdin.gets.chomp
+  filename.empty? ? 'students.csv' : filename
 end
 
 def show_students
@@ -41,33 +42,33 @@ def show_students
 end
 
 def input_students
-  puts "Please enter the student names"
-  puts "Hit return twice to finish"
-  name = STDIN.gets.chomp
+  puts 'Please enter the student names'
+  puts 'Hit return twice to finish'
+  name = $stdin.gets.chomp
 
-  while !name.empty? do
+  until name.empty?
     process_student(name, :november)
     puts "Now we have #{@students.count} students"
-    name = STDIN.gets.chomp
+    name = $stdin.gets.chomp
   end
 end
 
-def load_students(filename = "students.csv")
+def load_students(filename = 'students.csv')
   @students = []
-  File::open(filename, "r") do |file|
+  File.open(filename, 'r') do |file|
     CSV.foreach(file) do |row|
       process_student(row[0], row[1])
     end
   end
-    action_feedback("Loaded #{@students.count} from #{filename}")
+  action_feedback("Loaded #{@students.count} from #{filename}")
 end
 
 def process_student(name, cohort)
-  @students << {name: name, cohort: cohort.to_sym}
+  @students << { name: name, cohort: cohort.to_sym }
 end
 
-def save_students(filename = "students.csv")
-  File::open(filename, "w") do |file|
+def save_students(filename = 'students.csv')
+  File.open(filename, 'w') do |file|
     @students.each do |student|
       file.puts CSV.generate_line([student[:name], student[:cohort]])
     end
@@ -78,7 +79,7 @@ end
 def try_load_students(filename = ARGV.first)
   filename = ARGV.first
   if filename.nil?
-    load_students if File.exist?("students.csv")
+    load_students if File.exist?('students.csv')
   elsif File.exist?(filename)
     load_students(filename)
   else
@@ -88,13 +89,13 @@ def try_load_students(filename = ARGV.first)
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts 'The students of Villains Academy'
+  puts '-------------'
 end
 
 def print_students
   @students.each_with_index do |student, i|
-      puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -105,7 +106,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    selection = STDIN.gets.chomp
+    selection = $stdin.gets.chomp
     process(selection)
   end
 end
